1ac256489cd9f935cca5734b450f15b7a314a660
ajacobe <alniejacobe@gmail.com>
2013-07-01 19:48:06 +0800

    confirm delete of cache files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,8 +21,14 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Istudyante\AdminBundle\AdminBundle(),
+            new Istudyante\UserBundle\UserBundle(),
         );
 
+		if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+			$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+		}
         
 
         return $bundles;
